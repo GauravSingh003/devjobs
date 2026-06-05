c5a6282455f2fa15d0553d719a9ec30d5c6b18e1
GauravTechAu <gauravtechau@gmail.com>
2026-06-05 10:30:25 +0530

    feat: Navbar component created

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <>
+      <nav>
+        <h1>DevJobs</h1>
+            <ul>
+                <li>Home</li>
+                <li>Jobs</li>
+                <li>Saved</li>
+            </ul>
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
