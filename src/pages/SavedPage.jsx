b8cd629ddcec24b47a1a86b3c482bb25b470fad9
GauravTechAu <gauravtechau@gmail.com>
2026-06-07 17:20:52 +0530

    feat: saved job page complted

--- src/pages/SavedPage.jsx
+++ src/pages/SavedPage.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { BookmarkContext } from '../context/BookmarkContext'
+import JobCard from '../Components/JobCard';
 
 const SavedPage = () => {
+  const { bookmarks } = useContext(BookmarkContext);
   return (
     <>
-        <h1>Saved Page</h1>
+    <div className="job-container">
+        <h1>Saved jobs</h1>
+        {bookmarks.length === 0? (
+          <p>No Saveed Job yet!</p> 
+        ) : (
+          bookmarks.map((job) => (
+            <JobCard key={job.id} job= {job} />
+          ))
+        )}
+    </div>
     </>
   )
 }
